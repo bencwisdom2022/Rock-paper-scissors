eb61132c6a8b09fc20216f0aa2b258da9d621d5e
chukwuka <bencwisdom@gmail.com>
2024-07-28 20:48:18 +0100

    playRound function implemented

--- script.js
+++ script.js
@@ -6,42 +6,70 @@
 
 let randomChoice = Math.floor(Math.random() * 3 + 1);
 let computerSelect = Math.floor(Math.random() * 3 + 1);
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
 let user = 0;
 let computer = 1;
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  if (rock === randomChoice && rock === computerSelect) {
-    console.log('rock');
-  } else if (paper === randomChoice && paper === computerSelect) {
-    console.log('Paper');
-  } else if (scissors === randomChoice && scissors === computerSelect) {
-    console.log('Scissors');
+  if (1 === computerSelect) {
+    return 'rock';
+  } else if (2 === computerSelect) {
+    return 'Paper';
+  } else if (3 === computerSelect) {
+    return 'Scissors';
   }
 }
 
-let input1 = parseInt(prompt('Your turn to play'));
+let input1 = prompt('Your turn to play').toLowerCase();
+
+function humanChoiceLogic(input1) {
+  if (input1 === 'rock') {
+    return 1;
+  } else if (input1 === 'paper') {
+    return 2;
+  } else if (input1 === 'scissors') {
+    return 3;
+  }
+}
+
+let humanLogic = humanChoiceLogic(input1);
 
 function getHumanChoice(input1) {
-  if (rock === input1 && rock === randomChoice) {
-    console.log('rock');
-  } else if (paper === input1 && paper === randomChoice) {
-    console.log('Paper');
-  } else if (scissors === input1 && scissors === randomChoice) {
-    console.log('Scissors');
+  if ('rock' === input1) {
+    return 'rock';
+  } else if ('paper' === input1) {
+    return 'paper';
+  } else if ('scissors' === input1) {
+    return 'scissors';
   }
 }
 
-function playRound(humanChoice, computerChoice) {}
+function playRound(humanChoice, computerChoice) {
+  if (computerChoice === randomChoice && humanChoice === randomChoice) {
+    console.log(
+      `Computer guessed ${computerChoice} and user guessed ${humanChoice}, no winner`
+    );
+  } else if (computerChoice === randomChoice) {
+    console.log(
+      `Computer got ${computerChoice} and user got ${humanChoice}, computer won`
+    );
+  } else if (humanChoice === randomChoice) {
+    console.log(
+      `user got ${humanChoice} and computer got ${computerChoice}: user won`
+    );
+  } else if (humanChoice !== randomChoice && computerChoice !== randomChoice) {
+    console.log('You both are itiboribo');
+  }
+}
 
+let humanSelection = getHumanChoice(input1);
+let computerSelection = getComputerChoice();
+playRound(humanLogic, computerSelect);
 // getComputerChoice();
-getComputerChoice(computerSelect);
+// getComputerChoice(computerSelect);
 getHumanChoice(input1);
-console.log(computerSelect);
-console.log(input1);
+// console.log(computerSelect);
+// console.log(input1);
 console.log(randomChoice);
+console.log(computerSelect);
